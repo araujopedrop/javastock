ad118f65701c88cfc075203f5a31524181b4e530
gisemartinez <gise.cpna@gmail.com>
2013-08-15 18:14:17 -0300

    forma de hacerlo aplicacion de escritorio

--- src/frd/app/Main.java
+++ src/frd/app/Main.java
@@ -1,6 +1,11 @@
-package frd.app;
+	package frd.app;
 
+import java.awt.FlowLayout;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 	private static Scanner scanIn = new Scanner(System.in);
@@ -8,6 +13,24 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("*********** Iniciando ***********");
 		System.out.println("q:salir, u:listar usuarios, u-add:agregar usuario, u-del:borrar usuario");
+		//clase "app"
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlowLayout layout=new FlowLayout();
+		
+		frame.getContentPane().setLayout(layout);
+		frame.setTitle("java business");
+		frame.setLocation(100,200);
+		frame.setSize(300,200);
+		//ES OBLIGATORIO, con esto hago q cuando cierre la ventana tmb se pare el proceso
+		JLabel title= new JLabel("Sistema de riesgos");
+		JButton boton= new JButton("Benjamin Boton");
+		frame.getContentPane().add(title);
+		frame.getContentPane().add(boton);
+		
+		
+
+		frame.show();
 		
 		String response = scanIn.nextLine();
 		while( !"q".equalsIgnoreCase(response) ){
